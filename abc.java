d4a0c9c063ba8b1580e5ee1cbb24411b8bb1900e
Neeraja-VP <vpneerajavp@gmail.com>
2024-12-05 20:33:37 +0530

    made changes to abc.java

--- abc.java
+++ abc.java
@@ -1,5 +1,9 @@
 class abc{
   public static void main(String []args){
     System.out.println("Hello World");
+    int a=10;
+    int b=20;
+    int sum=a+b;
+    System.out.println("Sum"+sum);
   }
 }
